c63ae508fc80ee35304f174af4bd494b56594788
Ricardo Pasquini <rpasquini@gmail.com>
2020-04-11 17:52:26 -0300

    insert supply in producers

--- src/modules/producers/interfaces/supplyItem.ts
+++ src/modules/producers/interfaces/supplyItem.ts
@@ -3,6 +3,7 @@ import { Document, Schema } from 'mongoose';
 export interface SupplyItem extends Document {
   readonly name: string;
   readonly description: string;
+  readonly type: string;
 
   //readonly supply_id: Schema.Types.ObjectId;
   //readonly quantity: number;
